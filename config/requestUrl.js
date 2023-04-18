09e4c7a9310ac2e38f121e93df29f61fa64be0b7
Wnig <770225508@qq.com>
2023-04-18 17:42:52 +0800

    初始化

---
+++ config/requestUrl.js
@@ -0,0 +1,7 @@
+let restUrl = 'https://huilala.smartmapdt.com/huizhan/'; //正式
+// let restUrl = 'https://abc.smartmapdt.com/huizhan/'; //测试
+// let restUrl = 'http://192.168.1.161:8080/huizhan/'; //本地
+// let restUrl = 'http://192.168.1.141:8080/'; //本地
+
+
+export { restUrl }
